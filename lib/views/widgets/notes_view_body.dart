ad1549421c6ed7cec3a012a0eae1443c1c101aa2
Your Name <you@example.com>
2023-02-09 15:34:38 -0800

    create custom appbar

---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/custom_app_bar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: Column(children:const [
+        SizedBox(height: 40,),
+        CustomAppBar(),
+      ]),
+    );
+  }
+}
+
+
+
