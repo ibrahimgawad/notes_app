daf915121364c892d0086896e759eb777101e1b4
Your Name <you@example.com>
2023-02-10 15:29:05 -0800

    add custom button to bottom sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/constant.dart';
 import 'package:note_app/views/widgets/custom_text_filed.dart';
+
+import 'custom_add_button.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
@@ -10,10 +13,22 @@ class AddNoteBottomSheet extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 25),
       child: Column(
         children: const [
-          SizedBox(height: 40,),
-          CustomTextField(),
+          SizedBox(
+            height: 32,
+          ),
+          CustomTextField(hint: "Title"),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(hint: "content", maxLines: 5),
+            SizedBox(
+            height: 80,
+          ),
+          CustomAddButton(),
         ],
       ),
     );
   }
 }
+
+
---
+++ lib/views/widgets/custom_add_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../../constant.dart';
+
+class CustomAddButton extends StatelessWidget {
+  const CustomAddButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding:const EdgeInsets.symmetric(vertical: 12),
+      decoration:  BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Kprimarycolor,
+      ),
+      child: const Center(
+        child: Text(
+          "Add",
+          style: TextStyle(fontSize: 25,color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
