3e858fd81f395fe240bd019c333e58eb8d5a18f9
Your Name <you@example.com>
2023-02-09 22:20:22 -0800

    add fileds to notes bottom sheet

--- lib/views/widgets/custom_text_filed.dart
+++ lib/views/widgets/custom_text_filed.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:note_app/constant.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
+  const CustomTextField({super.key, required this.hint, this.maxLines = 1});
 
+  final String hint;
+
+  final int maxLines;
   @override
   Widget build(BuildContext context) {
     return TextField(
       cursorColor: Kprimarycolor,
+      maxLines: maxLines,
       decoration: InputDecoration(
-        
-        hintText: "Title",
+        hintText: hint,
         border: buildBorder(),
         enabledBorder: buildBorder(),
         focusedBorder: buildBorder(Kprimarycolor),
@@ -18,10 +21,10 @@ class CustomTextField extends StatelessWidget {
     );
   }
 
-  buildBorder([ color]) {
+  buildBorder([color]) {
     return OutlineInputBorder(
       borderRadius: BorderRadius.circular(12),
-      borderSide:  BorderSide(color:color ?? Colors.white),
+      borderSide: BorderSide(color: color ?? Colors.white),
     );
   }
 }
