405e07e4428666bdc99409d560ef8864996c8184
Your Name <you@example.com>
2023-02-10 16:20:49 -0800

    add custom(appbar& text filed )to edit note view

--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -3,15 +3,16 @@ import 'package:flutter/material.dart';
 import 'custom_icon_search.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
-
+  const CustomAppBar({super.key, required this.title, required this.icon});
+  final String title;
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children:const [
-        Text("Notes",style: TextStyle(fontSize: 28),),
-        CustomIconSearch(),
+      children: [
+        Text(title,style: const TextStyle(fontSize: 28),),
+        CustomIconSearch(icon: icon),
       ],
     );
   }
